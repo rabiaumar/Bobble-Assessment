3c28ada7abc8301a30d85e6115d75b306666fef5
Rabia Umar <44341727+rabiaumar@users.noreply.github.com>
2021-02-09 20:12:02 +0530

    Update wordsuggestion.py

--- #3 Word Suggestion/wordsuggestion.py
+++ #3 Word Suggestion/wordsuggestion.py
@@ -1,16 +1,45 @@
+# Importing Libraries
+
 import re
 from collections import Counter
 import pandas as pd
 import sys
 
 def words(text): return re.findall(r'\w+', text.lower())
+
+# Checking we are getting the supported arguments
+
 if(len(sys.argv) != 3):
     print("Invalid Input")
     sys.exit()
+""" 
+CSV Supported Format:
+word             frequency
+word1            frequency1
+....             ..........
+....             ..........
+....             ..........
+....             ..........
+
+"""
+# Reading the csv file with the help of location
+# Reading the csv file and creating a DataFrame from it
 a = pd.read_csv(str(sys.argv[1]))
+
+# Sorting the DataFrame with respect to frequency
+
 a = a.sort_values("frequency", ascending = False)
+
+# Resetting the redundant index values
+
 a.reset_index(drop = True, inplace = True)
+
+# Creating a counter with the words
+
 WORDS = Counter(list(a.word))
+
+# Storing the word from CLI into a variable
+
 inp1 = sys.argv[2]
 
 def P(word, N=sum(WORDS.values())): 
@@ -25,6 +54,8 @@ def candidates(word):
 def known(words): 
     return set(w for w in words if w in WORDS)
 
+# Creating different transpose and other methods for calculating the output
+
 def edits1(word):
     letters    = 'abcdefghijklmnopqrstuvwxyz'
     splits     = [(word[:i], word[i:])    for i in range(len(word) + 1)]
@@ -35,7 +66,10 @@ def edits1(word):
     return set(deletes + transposes + replaces + inserts)
 def edits2(word): 
     return (e2 for e1 in edits1(word) for e2 in edits1(e1))
+
+# Outputting 5 different word suggestions by iterating the loop 5 times.
+
 for outputs in range(6):
     temp = (correction(inp1))
-    del WORDS[temp]
+    del WORDS[temp]             # Deleting the already outputted  
     print(temp, end = " ")
